d453fa7f06539496a73c4fe9b7a24ec4225c7213
neostyle88 <makssimm1@gmail.com>
2023-08-07 12:45:54 +0200

    implement book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=10, choices=Cover.choices, default=Cover.HARD
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=4, decimal_places=2)
