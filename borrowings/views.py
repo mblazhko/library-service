245af6ee9f6112417906f0f17d8fb74ee642da04
neostyle88 <makssimm1@gmail.com>
2023-08-07 15:48:22 +0200

    add IsAuthenticated permissions for borrowings displaying

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -15,6 +16,17 @@ class BorrowingViewSet(
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "retrieve":
